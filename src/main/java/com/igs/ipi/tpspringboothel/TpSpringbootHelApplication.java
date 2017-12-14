a1308b75a3b304236f5033905760d1f015c87526
Nirgalz <znirgal@gmail.com>
2017-12-14 13:33:10 +0100

    first commit

---
+++ src/main/java/com/igs/ipi/tpspringboothel/TpSpringbootHelApplication.java
@@ -0,0 +1,12 @@
+package com.igs.ipi.tpspringboothel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSpringbootHelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpSpringbootHelApplication.class, args);
+	}
+}
---
+++ src/test/java/com/igs/ipi/tpspringboothel/TpSpringbootHelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.igs.ipi.tpspringboothel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TpSpringbootHelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
