6fffbd07eca4d5e83f8eefbb2a3437aa9793b7a2
Nirgalz <znirgal@gmail.com>
2017-12-14 17:54:13 +0100

    update

--- src/main/java/com/igs/ipi/tpspringboothel/controller/GameController.java
+++ src/main/java/com/igs/ipi/tpspringboothel/controller/GameController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -47,6 +48,30 @@ public class GameController{
                 .addObject("game", game)
                 .addObject("gameTable", game.getGameTable());
         return modelAndView;
+    }
+
+
+    @GetMapping("/game/drop/{id}")
+    public String updateGame(@PathVariable Long id){
+
+        int idz = id.intValue() - 1;
+
+        GameModel game = partie.getGm();
+        Integer[][] gameTable = game.getGameTable();
+        for (int i = 0 ; i< 6 ; i++){
+            if (gameTable[5][idz] == 0){
+                gameTable[5][idz] = 1;
+            }else
+            if (gameTable[i][idz] == 1) {
+                gameTable[i - 1 ][idz] = 1;
+            }
+        }
+
+
+        game.setGameTable(gameTable);
+        partie.setGm(game);
+
+        return "redirect:/game";
     }
 
 }
